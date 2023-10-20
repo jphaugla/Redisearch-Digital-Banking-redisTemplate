9b1efd2f83a460f0be0cfa10c3e7ff7b80326b30
Jason Haugland <jason.haugland@jason.haugland-QHWT01FW60>
2023-10-20 14:57:37 -0500

    parametize kafka connection and producer name

--- scripts/setEnv.sh
+++ scripts/setEnv.sh
@@ -1,4 +1,11 @@
+# redis enterprise example
+# export REDIS_HOST=redis-17794.jph.jphaugla.demo-rlec.redislabs.com
+# export REDIS_PORT=17794
 export REDIS_HOST=localhost
 export REDIS_PORT=6379
-export REDIS_PASSWORD=jasonrocks
+export REDIS_PASSWORD=redis123
 export USE_SSL=false
+# substitue in actual KAFKA_HOST internal IP
+# this will not run using a public ip.  Did not set up the advertized listeners in kafka to make this work
+export KAFKA_HOST=10.10.0.2
+export KAFKA_PORT=9092
