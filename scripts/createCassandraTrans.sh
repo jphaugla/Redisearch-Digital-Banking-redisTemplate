5879296253ba5f4ab59846843a014c176b051b88
Jason Haugland <jason.haugland@jason.haugland-QHWT01FW60>
2023-10-30 09:11:18 -0500

    fix docker version with cassandra broadcast env variable

--- scripts/createCassandraTrans.sh
+++ scripts/createCassandraTrans.sh
@@ -1,3 +1,3 @@
-export CQLSH_HOST=74.235.229.140
+export CQLSH_HOST=localhost
 cqlsh -e "create keyspace if not exists banking with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 } ;"
 cqlsh < trans.cql
